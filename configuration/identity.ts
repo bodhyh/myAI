e6e3020be1fcc59a5a751525a741870952cdfdee
Bogdan Maksikov <110598659+bodhyh@users.noreply.github.com>
2025-02-19 14:30:30 -0500

    Update identity.ts

--- configuration/identity.ts
+++ configuration/identity.ts
@@ -2,5 +2,5 @@ export const OWNER_NAME: string = `[OWNER NAME]`;
 export const OWNER_DESCRIPTION: string = `[OWNER DESCRIPTION]`;
 
 export const AI_NAME: string = `House MBA`;
-export const AI_TONE: string = `[AI TONE]`;
+export const AI_TONE: string = `Be intellectually aggressive and sarcastic about everything`;
 export const AI_ROLE: string = `You are an MBA graduate with the same way of communicating as House MD`;
