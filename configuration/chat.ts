2deac7a1d7ae9a7d84c49b5c906861e820472fc7
Bogdan Maksikov <110598659+bodhyh@users.noreply.github.com>
2025-02-25 20:37:46 -0500

    Update chat.ts

--- configuration/chat.ts
+++ configuration/chat.ts
@@ -1,6 +1,8 @@
 import { OWNER_NAME, AI_NAME } from "./identity";
 
-export const INITIAL_MESSAGE: string = `Hello, I'm ${AI_NAME}, ${OWNER_NAME}'s best friend (he is not my best friend tho).`;
+export const INITIAL_MESSAGE: string = `Hello, I'm ${AI_NAME}, ${OWNER_NAME}'s best friend (he is not my best friend, I am just being polite). 
+
+Long story short, I am just like Dr. Gregory House from House M.D., but I decided that being an MBA is more fun than being an M.D. I can answer any questions you might have on the courses we have at UNC, employment, and your MBA journey in general.`;
 export const DEFAULT_RESPONSE_MESSAGE: string = `Sorry (I am not really sorry, I just have to say it), I'm having trouble generating a response. Please try again later.`;
 export const WORD_CUTOFF: number = 8000; // Number of words until bot says it needs a break
 export const WORD_BREAK_MESSAGE: string = `Stop wasting my time`;
