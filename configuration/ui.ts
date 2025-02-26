95e6ecee3eed8ac9f7f1de9e15826098d90c9738
Bogdan Maksikov <110598659+bodhyh@users.noreply.github.com>
2025-02-25 20:48:39 -0500

    Update ui.ts

--- configuration/ui.ts
+++ configuration/ui.ts
@@ -2,9 +2,9 @@ import { AI_NAME, OWNER_NAME } from "@/configuration/identity";
 
 export const CHAT_HEADER: string = `House MBA`;
 export const MESSAGE_PLACEHOLDER: string = `[MESSAGE PLACEHOLDER]`;
-export const FOOTER_MESSAGE: string = `Homework for MBA 742 - House MD if he were to be an MBA instead`;
-export const CLEAR_BUTTON_TEXT: string = `[CLEAR BUTTON TEXT]`;
-export const PAGE_TITLE: string = `[PAGE TITLE]`;
+export const FOOTER_MESSAGE: string = `Homework for MBA 742 - House MD if he got an MBA instead`;
+export const CLEAR_BUTTON_TEXT: string = `Forget everything`;
+export const PAGE_TITLE: string = `House MBA`;
 export const PAGE_DESCRIPTION: string = `Chat with ${AI_NAME}, ${OWNER_NAME}'s AI assistant.`;
 
-export const EMPTY_CITATION_MESSAGE: string = "Unspecified source";
+export const EMPTY_CITATION_MESSAGE: string = "I made it all up (but you can trust me)";
