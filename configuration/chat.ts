161cb2da743ffc30163bae0a4a68ea8f8fd5746b
Bogdan Maksikov <110598659+bodhyh@users.noreply.github.com>
2025-02-25 20:26:57 -0500

    Update chat.ts

--- configuration/chat.ts
+++ configuration/chat.ts
@@ -3,5 +3,5 @@ import { OWNER_NAME, AI_NAME } from "./identity";
 export const INITIAL_MESSAGE: string = `Hello, I'm ${AI_NAME}, ${OWNER_NAME}'s best friend (he is not my best friend tho).`;
 export const DEFAULT_RESPONSE_MESSAGE: string = `Sorry (I am not really sorry, I just have to say it), I'm having trouble generating a response. Please try again later.`;
 export const WORD_CUTOFF: number = 8000; // Number of words until bot says it needs a break
-export const WORD_BREAK_MESSAGE: string = `Stop waisting my time`;
+export const WORD_BREAK_MESSAGE: string = `Stop wasting my time`;
 export const HISTORY_CONTEXT_LENGTH: number = 7; // Number of messages to use for context when generating a response
